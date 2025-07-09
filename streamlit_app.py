cab386f26d4a8af7c5d6fc85350c3a3a095e4157
Neko Takahara <111856834+takahara-knz@users.noreply.github.com>
2025-07-09 09:59:18 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,13 @@ st.set_page_config(layout="wide") # ページ設定（ワイド表示）
 from app import app1  # 今は app1 のみ
 
 st.sidebar.title("📚 おしえてねこちゃんメニュー")
-app_choice = st.sidebar.radio("機能を選択してください", [
-    "xxTESTCD/xxTEST検索（Findings系ドメイン検索）",
-    "機能2（準備中）",
-    "機能3（準備中）"
-])
+#app_choice = st.sidebar.radio("機能を選択してください", [
+#    "xxTESTCD/xxTEST検索（Findings系ドメイン検索）",
+#    "機能2（準備中）",
+#    "機能3（準備中）"
+#])
 
-if app_choice == "xxTESTCD/xxTEST検索（Findings系ドメイン検索）":
+#if app_choice == "xxTESTCD/xxTEST検索（Findings系ドメイン検索）":
     app1.run()
 #elif app_choice == "機能2（準備中）":
 #    app2.run()
