63a2b2837a877247af76ced2bbd0566bb21e1613
Neko Takahara <111856834+takahara-knz@users.noreply.github.com>
2025-07-09 09:01:53 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from app import app1, app2, app3  # 各アプリをモジュールとして読み込む
 
 st.sidebar.title("📚 おしえてねこちゃんメニュー")
-app_choice = st.sidebar.radio("機能を選択してください", ["xxTEST検索", "機能2", "機能3"])
+app_choice = st.sidebar.radio("機能を選択してください", ["xxTESTCD/xxTEST検索（Findings系ドメイン検索）", "機能2", "機能3"])
 
 if app_choice == "xxTESTCD/xxTEST検索（Findings系ドメイン検索）":
     app1.run()
