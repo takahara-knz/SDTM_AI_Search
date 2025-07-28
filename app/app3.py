a78ff380ece3dd1be8f5669e16fa538149b56221
Neko Takahara <111856834+takahara-knz@users.noreply.github.com>
2025-07-28 14:21:13 +0900

    Update app3.py

--- app/app3.py
+++ app/app3.py
@@ -8,7 +8,8 @@ def run_app():
 
     # dataのパスを取得
     current_dir = os.path.dirname(__file__)
-    excel_path = os.path.join(current_dir, "..", "data", "stdmig_v3.3_tables_和訳付き.xlsx")
+    # excel_path = os.path.join(current_dir, "..", "data", "stdmig_v3.3_tables_和訳付き.xlsx")
+    excel_path = "/mount/src/sdtm_ai_search/data/stdmig_v3.3_tables_和訳付き.xlsx"
     st.write("Excelファイルのパス:", excel_path)
     st.write("このファイル存在する？", os.path.exists(excel_path))
     # Excelファイルからシートごとに読み込む
